cf68a367017336f9a27febd43ba9a55e920eabd3
wytan12 <tanweiyin12@gmail.com>
2024-06-10 13:17:40 +0800

    refactoring

--- apps/dis_api.py
+++ apps/dis_api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from tools.general_utils import unix_to_datetime
+from tools.general_utils import unix_to_datetime, get_model_data
 import pandas as pd
 from apps.schemas import BuildingDataResponse, GeneralInput, KPICardResponse, MonitoringDataResponse
 from tools.code_dict import benchmark_dict
@@ -19,38 +19,6 @@ def get_all_building_name():
     name_list = df.name.to_list()
     code_list = df.code.to_list()
     return {"name_list": name_list, "code_list": code_list}
-
-@router.get('/predictData', response_model=dict)
-def get_model_data(model):
-    """
-    get the predicted data from Excel file based on the selected prediction model.
-    :param model: str = "LGBM" or "Chronos"
-    :return: dict of the dataset 
-    """
-    # initialize an empty DataFrame to store the combined data
-    df_full = pd.DataFrame()
-
-    # load the original dataset from an Excel file
-    df = pd.read_excel("store/clean_data.xlsx")
-    
-    # check the model selected to retrieve the respective dataset 
-    if model == "Chronos":
-        df_predict = pd.read_excel("store/clean_data_chronos.xlsx")
-    elif model == "LGBM":
-        df_predict = pd.read_excel("store/clean_data_lgbm.xlsx")
-    
-    unique_codes = list(set(df_predict.code.to_list()))
-
-    # combine the original data with predicted data for each code 
-    for code in unique_codes:
-        df_code = df[df["code"] == code]
-        df_pred = df_predict[df_predict["code"] == code]
-        # combine the row from both datasets
-        df_combine= pd.concat([df_code, df_pred],ignore_index=True)
-        # Append the combined data to df_full
-        df_full = pd.concat([df_full, df_combine], ignore_index=True)
-    
-    return df_full.to_dict(orient='records')
 
 @router.get('/buildingData', response_model=BuildingDataResponse)
 def get_building_data(payload: GeneralInput):
