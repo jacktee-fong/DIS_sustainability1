0dd33bbb78794754c84ef3b87e8c259712a77a89
wytan12 <tanweiyin12@gmail.com>
2024-06-12 00:08:02 +0800

    added explanation

--- apps/data_utils.py
+++ apps/data_utils.py
@@ -18,15 +18,18 @@ def get_model_data(model):
     elif model == "LGBM":
         df_predict = pd.read_excel("store/output/clean_data_lgbm.xlsx")
     
+    # get a list of unique codes from the predicted dataset
     unique_codes = list(set(df_predict.code.to_list()))
 
     # combine the original data with predicted data for each code 
     for code in unique_codes:
+        # filter the original data for the current code
         df_code = df[df["code"] == code]
+        # filter the predicted data (df_predict) for the current code
         df_pred = df_predict[df_predict["code"] == code]
-        # combine the row from both datasets
+        # combine the row from both datasets. ignore index to avoid duplicate indices
         df_combine= pd.concat([df_code, df_pred],ignore_index=True)
-        # Append the combined data to df_full
+        # Append the combined data to df_full. ignore index to avoid duplicate indices
         df_full = pd.concat([df_full, df_combine], ignore_index=True)
     
     return df_full.to_dict(orient='records')
