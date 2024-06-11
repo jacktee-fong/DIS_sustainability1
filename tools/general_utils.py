029b44a0f313fbf33934bc3214f77e9098b01e3f
wytan12 <tanweiyin12@gmail.com>
2024-06-11 22:23:21 +0800

    refactoring

--- tools/general_utils.py
+++ tools/general_utils.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import zoneinfo
-import pandas as pd
 
 
 def unix_to_datetime(x, tz_str):
@@ -14,33 +13,4 @@ def unix_to_datetime(x, tz_str):
     local_time = datetime.fromtimestamp(x, timezone)
     return local_time
 
-def get_model_data(model):
-    """
-    get the predicted data from Excel file based on the selected prediction model.
-    :param model: str = "LGBM" or "Chronos"
-    :return: dict of the dataset 
-    """
-    # initialize an empty DataFrame to store the combined data
-    df_full = pd.DataFrame()
 
-    # load the original dataset from an Excel file
-    df = pd.read_excel("store/output/clean_data.xlsx")
-    
-    # check the model selected to retrieve the respective dataset 
-    if model == "Chronos":
-        df_predict = pd.read_excel("store/output/clean_data_chronos.xlsx")
-    elif model == "LGBM":
-        df_predict = pd.read_excel("store/output/clean_data_lgbm.xlsx")
-    
-    unique_codes = list(set(df_predict.code.to_list()))
-
-    # combine the original data with predicted data for each code 
-    for code in unique_codes:
-        df_code = df[df["code"] == code]
-        df_pred = df_predict[df_predict["code"] == code]
-        # combine the row from both datasets
-        df_combine= pd.concat([df_code, df_pred],ignore_index=True)
-        # Append the combined data to df_full
-        df_full = pd.concat([df_full, df_combine], ignore_index=True)
-    
-    return df_full.to_dict(orient='records')
